0fab5f7953563437cc2957fcf7bc60fc99568fcf
Izurii <46232520+Izurii@users.noreply.github.com>
2018-12-29 05:15:13 -0200

    Update BayonettaHack.cpp

--- BayonettaHack/BayonettaHack.cpp
+++ BayonettaHack/BayonettaHack.cpp
@@ -21,7 +21,8 @@ DWORD FindPattern(DWORD dwInicio, DWORD dwFim, BYTE *arrayBytes, CHAR mascara[])
 	return 0;
 }
 
-DWORD WINAPI inicializa(PVOID ini) {
+DWORD WINAPI inicializa(PVOID ini)
+{
 
 	HANDLE p = GetCurrentProcess();
 	BYTE cheat[13] = { 0xC7, 0x46, 0x2C, 0x00, 0x00, 0xC8, 0x42, 0xF3, 0x0F, 0x58, 0x46, 0x2C, 0xE9 };
@@ -30,10 +31,8 @@ DWORD WINAPI inicializa(PVOID ini) {
 	void* alloc = VirtualAllocEx(p, NULL, sizeof(cheat) + 1, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	void * pa = alloc;
 	pa = static_cast<char*>(pa) + 0xD;
-	BYTE kk[4] = { 0xF7,  0xF6,  0xF8, 0xF9 };
 	char t[1024];
-	sprintf_s(t, "%0x", alloc);
-	int a = 0;
+	sprintf_s(t, "%0x", alloc); 
 
 	while (true)
 	{
@@ -43,30 +42,30 @@ DWORD WINAPI inicializa(PVOID ini) {
 		if (GetAsyncKeyState(VK_F3))
 		{
 
+			
+			MessageBoxA(NULL, t, "Address of AllocMem", NULL);
 
 
-			WriteProcessMemory(p, LPVOID(alloc), &cheat, sizeof(cheat), nullptr);
-			MessageBoxA(NULL, t, "oi", NULL);
-
-
-		}
-
-		if (GetAsyncKeyState(VK_F4)) {
-
-		//	void* x1 = (void*)hp;
-		//	void* x2 = alloc;
 			DWORD x1 = (DWORD)alloc + 0xC;
 			DWORD x2 = (hp + 0x5) - x1 - 0x5;
 			void *x3 = (void*)x2;
+			DWORD x4 = (DWORD)alloc;
+			DWORD x5 = x4 - hp - 0x5;
+			void *x6 = (void*)x5;
+
+
+			WriteProcessMemory(p, LPVOID(alloc), &cheat, sizeof(cheat), nullptr);
 			WriteProcessMemory(p, LPVOID(pa), &x3, sizeof(x3), nullptr);
+			WriteProcessMemory(p, LPVOID(hp), &ori, sizeof(ori), nullptr);
+			WriteProcessMemory(p, LPVOID(hp+0x1), &x6, sizeof(x6), nullptr);
+
 
 		}
 
 	}
 
-
-
 	return 0;
+
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
@@ -85,4 +84,3 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	}
 	return TRUE;
 }
-
