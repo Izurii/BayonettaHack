8d54002c9d88c2c054aa06a61b4a5d12ca6055c3
Izurii <46232520+Izurii@users.noreply.github.com>
2019-01-01 01:11:55 -0200

    Update dllmain.cpp

--- BayonettaHack/dllmain.cpp
+++ BayonettaHack/dllmain.cpp
@@ -17,11 +17,15 @@ DWORD WINAPI main(PVOID ini)
 	BYTE haloCheat[] = { 0x90, 0xE9 }; 
 	BYTE healthCheat[] = { 0x90, 0xE9 };
 	BYTE witchTimeCheat[] = { 0xC7, 0x86, 0x5C, 0x5D, 0x09, 0x00, 0x00, 0x00, 0xC8, 0x42, 0xF3, 0x0F, 0x10, 0x96, 0x5C, 0x5D, 0x09, 0x00, 0xE9 };
+	BYTE magicCheat1[] = { 0x90, 0xE9 };
+	BYTE magicCheat2[] = { 0x90, 0xE9 };
 
 	//Actual original/patterns codes
 	BYTE goldBytes[] = { 0x29, 0x81, 0xE4, 0xF5, 0x00, 0x00 };
 	BYTE healthBytes[] = { 0x89, 0x86, 0x08, 0x35, 0x09, 0x00 };
 	BYTE witchTimeBytes[] = { 0xF3, 0x0F, 0x10, 0x96, 0x5C, 0x5D, 0x09, 0x00 };
+	BYTE magicBytes1[] = { 0xF3, 0x0F, 0x5C, 0x05, 0xDC, 0x0D, 0xDA, 0x00 };
+	BYTE magicBytes2[] = { 0xF3, 0x0F, 0x5C, 0x05, 0x58, 0x4B, 0xDA, 0x00, 0x0F, 0x57, 0xC9, 0x0F, 0x2F, 0xC8, 0xF3, 0x0F, 0x11, 0x05 };
 
 	//Other things lol
 
@@ -48,7 +52,7 @@ DWORD WINAPI main(PVOID ini)
 	//Health
 	DWORD healthFirstAddress = gameBaseAddress + 0x0567AB8C;
 	DWORD healthSecondAddress = gameBaseAddress + 0x008235F8;
-	char healthFirstOffset[] = { 0x0, 0x168, 0x8, 0x4, 0x0, 0x98 };
+	char healthFirstOffset[] = { 0x0, 0x168, 0x8, 0x4, 0x0, 0x98 }; //I'm goinig to use this... in the future
 	char healthSecondOffset[] = { 0xC };
 	unsigned int healthFirstFinalAddress, healthSecondFinalAddress;
 	int healthMax = 8000;
@@ -59,7 +63,16 @@ DWORD WINAPI main(PVOID ini)
 	void* witchTimeAddressOri = 0;
 	void* witchTimeMemAddressOri = 0;
 
-	
+	//Magic
+	DWORD magicAddress = gameBaseAddress + 0x004BC82C;
+	BYTE magicOffset[] = { 0x14 };
+	unsigned int magicFinalAddress;
+	float magicMax = 1200;
+	void* magicMemAddressOri1 = 0;
+	void* magicMemAddressOri2 = 0;
+	void* magicScriptAddressOri1 = 0;
+	void* magicScriptAddressOri2 = 0;
+
 	//--------------------------------------------------------------------------------
 
 
@@ -86,7 +99,12 @@ DWORD WINAPI main(PVOID ini)
 
 	ReadProcessMemory(p, LPVOID(healthSecondAddress), &temp, sizeof(temp), nullptr);
 
-	healthSecondFinalAddress = temp + 0xC;
+	healthSecondFinalAddress = temp + healthSecondOffset[0];
+
+	//Magic
+
+	ReadProcessMemory(p, LPVOID(magicAddress), &temp, sizeof(temp), nullptr);
+	magicFinalAddress = temp + magicOffset[0];
 
 	//--------------------------------------------------------------------------------
 
@@ -99,13 +117,19 @@ DWORD WINAPI main(PVOID ini)
 		if (GetAsyncKeyState(VK_F1))
 		{
 
+			//Allocate Memory
+
 			void* MemAddress = allocMem(p, haloCheat + 0x5);
-			void* MemAddressOff = static_cast<char*>(MemAddress) + 0x2;
-			
+			void* MemAddressOff = static_cast<char*>(MemAddress) + sizeof(haloCheat);
+
+			//Find the address of original code
+
 			DWORD scriptGold = FindPattern(0x00501000, 6000, goldBytes, "xxxxxx");
 
+			//Calculate the jmp bytes
+
 			void* jmpToCheat = calJmp(MemAddress, scriptGold, 1, NULL);
-			void* jmpOfReturn = calJmp(MemAddress, scriptGold, 0, sizeof(haloCheat));
+			void* jmpOfReturn = calJmp(static_cast<char*>(MemAddress) - 0x3, scriptGold, 0, sizeof(haloCheat));
 
 			if (status == 0)
 			{
@@ -129,7 +153,7 @@ DWORD WINAPI main(PVOID ini)
 
 				WriteProcessMemory(p, LPVOID(scriptGold), &jmpInstruction, sizeof(jmpInstruction), nullptr);
 				WriteProcessMemory(p, LPVOID(scriptGold + 0x1), &jmpToCheat, sizeof(jmpToCheat), nullptr);
-				WriteProcessMemory(p, LPVOID(scriptGold + 0x5), &nopInstruction, sizeof(nopInstruction), nullptr);
+				putNopes(p, scriptGold, 1);
 
 			}
 			else
@@ -148,11 +172,17 @@ DWORD WINAPI main(PVOID ini)
 
 		if (GetAsyncKeyState(VK_F2))
 		{
-			
-			void* MemAddress = allocMem(p, healthCheat+0x5);
-			void* MemAddressOff = static_cast<char*>(MemAddress) + 0x2;
+
+			//Allocate Memory
+
+			void* MemAddress = allocMem(p, healthCheat + 0x5);
+			void* MemAddressOff = static_cast<char*>(MemAddress) + sizeof(healthCheat);
+
+			//Find the address of original code
 
 			DWORD scriptHealth = FindPattern(0x009D4000, 6000, healthBytes, "xxxxxx");
+
+			//Calculate the jmp bytes
 
 			void* jmpToCheat = calJmp(MemAddress, scriptHealth, 1, NULL);
 			void* jmpOfReturn = calJmp(MemAddress, scriptHealth, 0, sizeof(healthCheat));
@@ -180,7 +210,7 @@ DWORD WINAPI main(PVOID ini)
 
 				WriteProcessMemory(p, LPVOID(scriptHealth), &jmpInstruction, sizeof(jmpInstruction), nullptr);
 				WriteProcessMemory(p, LPVOID(scriptHealth + 0x1), &jmpToCheat, sizeof(jmpToCheat), nullptr);
-				WriteProcessMemory(p, LPVOID(scriptHealth + 0x5), &nopInstruction, sizeof(nopInstruction), nullptr);
+				putNopes(p, scriptHealth, 1);
 
 			}
 			else {
@@ -193,19 +223,25 @@ DWORD WINAPI main(PVOID ini)
 				VirtualFreeEx(p, healthMemAddressOri, sizeof(healthCheat + 0x5), MEM_DECOMMIT);
 
 			}
-		
+
 		}
 
 		if (GetAsyncKeyState(VK_F3))
 		{
 
+			//Allocate Memory
+
 			void* MemAddress = allocMem(p, witchTimeCheat + 0x5);
 			void* MemAddressOff = static_cast<char*>(MemAddress) + sizeof(witchTimeCheat);
 
+			//Find the address of original code
+
 			DWORD scriptWitchTime = FindPattern(0x009E1000, 6000, witchTimeBytes, "xxxxxxxx");
-			
+
+			//Calculate the jmp bytes
+
 			void* jmpToCheat = calJmp(MemAddress, scriptWitchTime, 1, NULL);
-			void* jmpOfReturn = calJmp((static_cast<char*>(MemAddressOff)-0x1), scriptWitchTime, 0, 0x8);
+			void* jmpOfReturn = calJmp((static_cast<char*>(MemAddressOff) - 0x1), scriptWitchTime, 0, 0x8);
 
 			if (status == 0)
 			{
@@ -224,9 +260,7 @@ DWORD WINAPI main(PVOID ini)
 
 				WriteProcessMemory(p, LPVOID(scriptWitchTime), &jmpInstruction, sizeof(jmpInstruction), nullptr);
 				WriteProcessMemory(p, LPVOID(scriptWitchTime + 0x1), &jmpToCheat, sizeof(jmpToCheat), nullptr);
-				WriteProcessMemory(p, LPVOID(scriptWitchTime + 0x5), &nopInstruction, sizeof(nopInstruction), nullptr);
-				WriteProcessMemory(p, LPVOID(scriptWitchTime + 0x6), &nopInstruction, sizeof(nopInstruction), nullptr);
-				WriteProcessMemory(p, LPVOID(scriptWitchTime + 0x7), &nopInstruction, sizeof(nopInstruction), nullptr);
+				putNopes(p, scriptWitchTime, 3);
 
 
 			}
@@ -238,6 +272,77 @@ DWORD WINAPI main(PVOID ini)
 
 				WriteProcessMemory(p, LPVOID(witchTimeAddressOri), &witchTimeBytes, sizeof(witchTimeBytes), nullptr);
 				VirtualFreeEx(p, witchTimeMemAddressOri, sizeof(witchTimeCheat + 0x5), MEM_DECOMMIT);
+
+			}
+
+		}
+
+		if (GetAsyncKeyState(VK_F4))
+		{
+
+			//Allocate Memory
+
+			void* MemAddress1 = allocMem(p, (magicCheat1 + 0x5));
+			void* MemAddress2 = allocMem(p, (magicCheat2 + 0x5));
+			void* MemAddressOff1 = static_cast<char*>(MemAddress1) + sizeof(magicCheat1);
+			void* MemAddressOff2 = static_cast<char*>(MemAddress2) + sizeof(magicCheat2);
+
+			//Find the address of original code
+
+			DWORD scriptMagic1 = FindPattern(0x00501000, 6000, magicBytes1, "xxxxxxxx");
+			DWORD scriptMagic2 = FindPattern(0x00502000, 6000, magicBytes2, "xxxxxxxxxxxxxxxxxx");
+
+			//Calculate the jmp bytes
+
+			void* jmpToCheat1 = calJmp(MemAddress1, scriptMagic1, 1, NULL);
+			void* jmpToCheat2 = calJmp(MemAddress2, scriptMagic2, 1, NULL);
+			void* jmpOfReturn1 = calJmp((static_cast<char*>(MemAddressOff1) - 0x1), scriptMagic1, 0, sizeof(magicBytes1));
+			void* jmpOfReturn2 = calJmp((static_cast<char*>(MemAddressOff2) + 0x9), scriptMagic2, 0, sizeof(magicBytes2));
+
+			if (status == 0)
+			{
+
+				//As you see...
+
+				status = 1;
+				magicScriptAddressOri1 = reinterpret_cast<void*>(scriptMagic1);
+				magicScriptAddressOri2 = reinterpret_cast<void*>(scriptMagic2);
+				magicMemAddressOri1 = MemAddress1;
+				magicMemAddressOri2 = MemAddress2;
+
+				//Max the magic
+
+				WriteProcessMemory(p, LPVOID(magicFinalAddress), &magicMax, sizeof(magicMax), nullptr);
+
+				//Write in allocated memory
+
+				WriteProcessMemory(p, LPVOID(MemAddress1), &magicCheat1, sizeof(magicCheat1), nullptr);
+				WriteProcessMemory(p, LPVOID(MemAddressOff1), &jmpOfReturn1, sizeof(jmpOfReturn1), nullptr);
+
+				WriteProcessMemory(p, LPVOID(MemAddress2), &magicCheat2, sizeof(magicCheat2), nullptr);
+				WriteProcessMemory(p, LPVOID(MemAddressOff2), &jmpOfReturn2, sizeof(jmpOfReturn2), nullptr);
+
+				//c0d1ng caverna
+
+				WriteProcessMemory(p, LPVOID(scriptMagic1), &jmpInstruction, sizeof(jmpInstruction), nullptr);
+				WriteProcessMemory(p, LPVOID(scriptMagic1 + 0x1), &jmpToCheat1, sizeof(jmpToCheat1), nullptr);
+				putNopes(p, scriptMagic1, 3);
+
+				WriteProcessMemory(p, LPVOID(scriptMagic2), &jmpInstruction, sizeof(jmpInstruction), nullptr);
+				WriteProcessMemory(p, LPVOID(scriptMagic2 + 0x1), &jmpToCheat2, sizeof(jmpToCheat2), nullptr);
+				putNopes(p, scriptMagic2, 3);
+
+			}
+			else {
+
+				status = 0;
+
+				//Return original code in game memory and deallocate the memory allocated by allocMem
+
+				WriteProcessMemory(p, LPVOID(magicScriptAddressOri1), &magicBytes1, sizeof(magicBytes1), nullptr);
+				WriteProcessMemory(p, LPVOID(magicScriptAddressOri2), &magicBytes2, sizeof(magicBytes2), nullptr);
+				VirtualFreeEx(p, magicMemAddressOri1, sizeof(magicCheat1 + 0x5), MEM_DECOMMIT);
+				VirtualFreeEx(p, magicMemAddressOri2, sizeof(magicCheat2 + 0x5), MEM_DECOMMIT);
 
 			}
 
